d589eff792f4891dac674ffe1873856e0777f905
Maryam -004 <maryameshal876@gmail.com>
2026-01-03 05:00:04 -0800

    Implemented countLectures() function to count lectures per course

---
+++ count_lecture.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "lecture.h"
+
+using namespace std;
+
+void countLectures() {
+
+    ifstream file("LectureRecord.txt");
+    string line;
+
+    if(!file) {
+        cout << "LectureRecord.txt file not found!\n";
+        return;
+    }
+
+    int totalLectures;
+    cout << "Enter total lectures required in semester: ";
+    cin >> totalLectures;
+
+    string courseToCount;
+    cout << "Enter Course ID: ";
+    cin >> courseToCount;
+
+    int subjectCount = 0;
+
+    while(getline(file, line)) {
+        stringstream ss(line);
+        string lectureID, courseID;
+
+        getline(ss, lectureID, ',');
+        getline(ss, courseID, ',');
+
+        if(courseID == courseToCount) {
+            subjectCount++;
+        }
+    }
+
+    int remaining = totalLectures - subjectCount;
+
+    cout << "\nLectures conducted for " << courseToCount << ": "
+         << subjectCount << endl;
+    cout << "Remaining lectures: " << remaining << endl;
+
+    file.close();
+}
