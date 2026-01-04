48052aba20eb1147c1af000f36c9c0f85ff273b4
Sidra123-kanwal <kanwalsidra2006@gmail.com>
2026-01-04 06:48:10 -0800

    Update lecture.h

--- src/lecture.h
+++ src/lecture.h
@@ -1,8 +1,17 @@
 #ifndef LECTURE_H
 #define LECTURE_H
 
+// ===============================
+//        MARYAM (Lecture Module)
+// ===============================
 void addLecture();
 void readLectures();
 void countLectures();
+
+// ===============================
+//        SIDRA (Student Module)
+// ===============================
+void addstudent()
+void readstudent()
 
 #endif
