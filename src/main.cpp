144bf07a986d5fdee56fda6283ab4559d6de4a93
Sidra123-kanwal <kanwalsidra2006@gmail.com>
2026-01-04 07:57:39 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,68 @@ int main() {
     int choice;
 
     do {
-        cout << "\n===== Lecture Management System =====\n";
-        cout << "1. Add Lecture Record\n";
-        cout << "2. View Lecture Records\n";
-        cout << "3. Count Lectures\n";
-        cout << "4. Exit\n";
+        cout << "\n========= UNIVERSITY MANAGEMENT SYSTEM =========\n";
+        cout << "1. Add Lecture\n";
+        cout << "2. View Lectures\n";
+        cout << "3. Count Lectures (Course wise)\n";
+        cout << "4. Add Course\n";
+        cout << "5. View Courses\n";
+        cout << "6. Add Teacher\n";
+        cout << "7. View Teachers\n";
+        cout << "8. Add Student\n";
+        cout << "9. View Students\n";
+        cout << "0. Exit\n";
+        cout << "==============================================\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
-        if(choice == 1)
-            addLecture();
-        else if(choice == 2)
-            readLectures();
-        else if(choice == 3)
-            countLectures();
-        else if(choice == 4)
-            cout << "Exiting program...\n";
-        else
-            cout << "Invalid choice! Try again.\n";
+        switch(choice) {
 
-    } while(choice != 4);
+            case 1:
+                addLecture();
+                break;
+
+            case 2:
+                readLectures();
+                break;
+
+            case 3:
+                countLectures();
+                break;
+
+            case 4:
+                course();
+                break;
+
+            case 5:
+                readcourse();
+                break;
+
+            case 6:
+                addteacher();
+                break;
+
+            case 7:
+                readteacher();
+                break;
+
+            case 8:
+                addstudent();
+                break;
+
+            case 9:
+                readstudent();
+                break;
+
+            case 0:
+                cout << "\nExiting program... Thank you!\n";
+                break;
+
+            default:
+                cout << "\nInvalid choice! Try again.\n";
+        }
+
+    } while(choice != 0);
 
     return 0;
 }
